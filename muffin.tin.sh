0da60613cf1049d828f12aea318bbcee232bd12f
Ａｐｐｌｅ Ｆｒｉｔｔｅｒ <125221355+apple-fritter@users.noreply.github.com>
2023-07-20 16:47:45 -0700

    Update muffin.tin.sh

--- muffin.tin.sh
+++ muffin.tin.sh
@@ -1,25 +1,48 @@
 #!/bin/bash
 
-# Change to the home directory
-cd ~
+# Replace 'places.sqlite' with your database file path
+db_file="places.sqlite"
 
-# Prompt the user for the path to the places file
-read -p "Enter the path to the places file, or press enter to search for it automatically: " PLACES_PATH
-
-# If the user didn't enter a path, search for the places file in the default Firefox profile directory
-if [ -z "$PLACES_PATH" ]; then
-  FIREFOX_PROFILE=$(find ~/.mozilla/firefox -maxdepth 1 -type d -name "*.default*" | head -n 1)
-  PLACES_PATH="$FIREFOX_PROFILE/places.sqlite"
+# Check if the database file exists
+if [ ! -f "$db_file" ]; then
+  echo "Error: Database file '$db_file' not found."
+  exit 1
 fi
 
-# Get a list of all the tables in the database
-TABLES=$(sqlite3 "$PLACES_PATH" ".tables")
+# Step 1: Extract table names using sqlite3
+table_names=$(sqlite3 "$db_file" ".tables")
 
-# Set the output mode to XML
-echo ".mode xml"
+# Check if any tables are found in the database
+if [ -z "$table_names" ]; then
+  echo "No tables found in the database."
+  exit 1
+fi
 
-# Loop over each table and output its contents to an XML file
-for table in $TABLES; do
-  echo ".output ${table}.xml"
-  echo "SELECT * FROM ${table};"
-done | sqlite3 "$PLACES_PATH"
+# Set the table names as an environment variable
+export TABLE_NAMES="$table_names"
+
+# Create a new folder in the user's home directory with the specified subdirectory name
+timestamp=$(date +"%Y%m%d%H%M%S")
+output_folder="$HOME/places_tables_$timestamp"
+mkdir -p "$output_folder"
+
+# Step 2: Python code to export tables to XML files in the new folder
+python3 << END_PYTHON_SCRIPT
+import os
+import sqlite3
+import pandas as pd
+
+db_file = "$db_file"
+table_names = os.environ.get("TABLE_NAMES")
+tables = table_names.split()
+
+connection = sqlite3.connect(db_file)
+
+for table in tables:
+    query = f"SELECT * FROM {table}"
+    df = pd.read_sql_query(query, connection)
+    xml_filename = os.path.join("$output_folder", f"{table}.xml")
+    df.to_xml(xml_filename, index=False)
+
+connection.close()
+END_PYTHON_SCRIPT
